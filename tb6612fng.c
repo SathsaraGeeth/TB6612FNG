2c94c11af052eb6f43eba84aaf13297765a68c71
Geeth Sathsara <geethwa03@gmail.com>
2025-01-06 16:13:22 +0530

    initial commit

---
+++ tb6612fng.c
@@ -0,0 +1,47 @@
+#include "tb6612.h"
+#include <stdio.h>
+
+void (*set_pin)(int pin, int state) = NULL;
+void (*start_pwm)(int pwm, int frequency) = NULL;
+void (*noop)(int time) = NULL;
+
+void tb6612_driver_init(tb6612_driver* driver){
+    load_settings DEFALUT_SETTINGS = {0, 0};
+    driver->LOAD1.settings = DEFALUT_SETTINGS;
+    driver->LOAD2.settings = DEFALUT_SETTINGS;
+    set_pin(driver->STBY, 1);
+}
+void load_init(load* load){
+    if (!load->settings.INV_POL){
+        set_pin(load->IN1, 1);
+        set_pin(load->IN2, 0);
+    } else{
+        set_pin(load->IN1, 0);
+        set_pin(load->IN2, 1);
+    };
+    if (load->settings.FPWM <= MAX_FPWM){
+        start_pwm(load->PWM, load->load_settings.FPWM);
+    } else{
+        start_pwm(loa->PWM, MAX_FPWM); // if the FPWM is too high cap it at the MAX_FPWM
+    }
+}
+void update_load_settings(load* load, load_settings* new_settings){
+    load->settings = *new_settings;
+    load_stop(load* load);
+    noop(DEAD_TIME);
+    load_init(load* load);
+}
+void load_short_break(load* load){
+    load_stop(load* load);
+    noop(DEAD_TIME);
+    set_pin(load->IN1, 1);
+    set_pin(load->IN2, 1);
+}
+void load_stop(load* load){
+    set_pin(load->IN1, 0);
+    set_pint(load->IN2, 0);
+    set_pin(load->PWM, 1); // or start_pwm(load->PWM, 0); 
+}
+void tb6612_driver_off(tb6612_driver* driver){
+    HAL_GPIO_WritePin(/* port */, driver->STBY, GPIO_PIN_RESET);
+}
---
+++ tb6612fng.h
@@ -0,0 +1,76 @@
+#ifndef TB6612_H
+#define TB6612_H
+
+
+#define MAX_FPWM 100000
+#define DEAD_TIME 2
+
+/* function pointers for hardware control */
+extern void (*set_pin)(int pin, int state); // state -> 1: ON, 0: OFF
+extern void (*start_pwm)(int pin, int frequency); // frequency in Hz
+/* NOOP */
+extern void (*noop)(int time); // time in nano seconds
+
+typedef struct {
+    int FPWM;
+    int INV_POL;
+} load_settings;
+
+typedef struct {
+    int IN1;
+    int IN2;
+    int PWM;
+    load_settings settings;
+} load;
+
+typedef struct {
+    load LOAD1;
+    load LOAD2;
+    int STBY;
+} tb6612_driver;
+
+/**
+ * @brief Initializes the driver with default settings.
+ * 
+ * @param driver Pointer to the tb6612_driver struct.
+ */
+void tb6612_driver_init(tb6612_driver* driver);
+
+/**
+ * @brief Initializes the load on with specified settings.
+ * 
+ * @param load Pointer to the load struct.
+ */
+void load_init(load* load);
+
+/**
+ * @brief Updates the load's settings.
+ * 
+ * @param load Pointer to the load struct.
+ * @param new_settings Pointer to the new settings struct.
+ */
+void update_load_settings(load* load, load_settings* new_settings);
+
+/**
+ * @brief Short brakes the load.
+ * 
+ * @param load Pointer to the load struct.
+ */
+void load_short_break(load* load);
+
+/**
+ * @brief Stops the load -> OFF(High Impedence).
+ * 
+ * @param load Pointer to the load struct.
+ */
+void load_stop(load* load);
+
+/**
+ * @brief Forces the TB6612 driver in the standby mode -> OFF(High Impedence).
+ * 
+ * @param driver Pointer to the TB6612 driver structure.
+ */
+void tb6612_driver_off(tb6612_driver* driver);
+
+
+#endif // TB6612_H
